7ca1991075d04d7ba460c2c67f11b2f09cc38af9
Iwona Jaśtak <i.jastak@madkom.pl>
2020-05-14 16:28:23 +0200

    working backend

---
+++ my-cluster/my-backend/index.js
@@ -0,0 +1,27 @@
+const { v4: uuidv4 } = require('uuid');
+
+const express = require('express');
+
+const keys = require('./keys.js');
+
+const app = express();
+
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const appId = uuidv4();
+const appPort = 5000;
+
+
+app.get('/', (req, res) => {
+  res.send(`[${appId}] ${keys.initMessage}`)
+});
+
+app.listen(appPort, err => {
+  console.log(`Backend listening on port ${appPort}`)
+});
